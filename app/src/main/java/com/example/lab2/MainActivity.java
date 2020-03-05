05d01b6614d30f17161d8d6c2aee7af16573f110
Victor Segura <viko_1995@2hotmail.com>
2020-03-04 19:11:10 -0600

    commit 2

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+
+
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MessageActivity.class);
+        EditText editText = findViewById(R.id.text_message);
+        intent.putExtra("MESSAGE",  editText.getText().toString());
+        startActivity(intent);
+
     }
 }
